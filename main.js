e0105992a0feeafca45caa7e55cddd52f9350a8b
Andrii Prokopovych <a.prokopovych@visartech.com>
2023-10-11 12:04:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function init() {
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
